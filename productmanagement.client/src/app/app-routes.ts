0f0e9e39c14ebc9637b9971428a0c26706a1121b
Renan do Nascimento Sanches <renan.sanches@exception.com.br>
2025-03-24 09:13:44 -0300

    lazy load module

--- productmanagement.client/src/app/app-routes.ts
+++ productmanagement.client/src/app/app-routes.ts
@@ -1,10 +1,9 @@
 import { Routes } from '@angular/router';
 
-import { OrderFormComponent } from './features/order/pages/order-form/order-form.component';
 import { OrderListComponent } from './features/order/pages/order-list/order-list.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/orders', pathMatch: 'full' },
   { path: 'orders', component: OrderListComponent },
-  { path: 'orders/new', component: OrderFormComponent }
+  { path: 'orders/new', loadComponent: () => import('./features/order/pages/order-form/order-form.component').then(m => m.OrderFormComponent) }
 ];
--- productmanagement.client/src/app/features/order/models/order.dto.ts
+++ productmanagement.client/src/app/features/order/models/order.dto.ts
@@ -1,16 +1,6 @@
 import { OrderItemDto } from './order-item.dto';
 import { OrderStatus } from './order-status.enum';
 
-export interface PaginatedResponseDTO<T> {
-  hasNext: boolean;
-  hasPrevious: boolean; 
-  items: T[];
-  pageNumber: number;
-  pageSize: number;
-  totalCount: number;
-  totalPages: number;
-}
-
 export interface OrderDto {
   id: string;
   orderDate: Date;
@@ -22,20 +12,3 @@ export interface OrderDto {
   createdAt: Date;
   updatedAt?: Date;
 }
-
-
-
-export interface CreateOrderDto {
-  customerName: string;
-  customerEmail: string;
-  orderItems: CreateOrderItemDto[];
-}
-
-export interface CreateOrderItemDto {
-  productId: string;
-  quantity: number;
-}
-
-export interface UpdateOrderStatusDto {
-  status: OrderStatus;
-} 
